56614efa0a59d5dfa3121cc9c343eca708cb99e1
yelm-212 <21yrshin@naver.com>
2025-01-17 09:53:26 +0900

    chore: ElementPlus 관련 의존성 추가

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementPlus from 'element-plus'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'element-plus/dist/index.css'
 
 const app = createApp(App)
 app.use(router)
 app.use(store)
+app.use(ElementPlus)
 app.mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,22 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require('@vue/cli-service');
+const AutoImport = require('unplugin-auto-import/webpack').default;
+const Components = require('unplugin-vue-components/webpack').default;
+const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
+
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer : {
     port: 8081
+  },
+  lintOnSave: false,
+  configureWebpack: {
+    plugins: [
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
+      }),
+    ],
   }
 })
